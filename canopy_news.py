915e5da140261d8b3a5bc1a16e1a215c5e834229
nicholaswiseman <nicholas.jw.wiseman@gmail.com>
2018-10-11 10:58:53 -0230

    changed string formatting to work with older versions of python

--- canopy_news.py
+++ canopy_news.py
@@ -2,6 +2,7 @@ import bs4 as bs
 import urllib.request
 from my_email_script import send_email
 import time
+import datetime
 
 #All the news release urls start with the same string.
 #Look for that string in the url to see if its news.
@@ -32,24 +33,29 @@ def is_recent(url):
 
 def append_url(url):
 	file = open('urls.txt','a')
-	file.write(f"\n{url.get('href')}")
+	file.write('\n')
+	file.write(url.get('href'))
 	file.close()
 
 if __name__ == '__main__':
 	while 1:
-		#grab the html and parse it
-		source = urllib.request.urlopen('https://www.canopygrowth.com/investors/news-releases/').read()
-		soup = bs.BeautifulSoup(source,'lxml')
-		
-		#for each hyperlink we check if its news and if we've seen it before
-		for url in soup.find_all('a'):
-			if is_news(url) and is_recent(url):
-				#here we send an email if its new. For this to work you will need the pw
-				try:
-					msg = f"Hey Josh,\n\nA news release from Canopy Growth has been posted:\n\n{url.get('href')}\n\nYour friend,\nCanopy Updates"
-					send_email('canopy.updates@gmail.com','nickandjosh','jlevesque11@gmail.com','Canopy Growth News Release',msg)
-					append_url(url)
-					print("E-mail sent!")
-				except:
-					print("Error sending email! Check e-mail and password and run again!\n")
-		time.sleep(30)		
+		try:
+			#grab the html and parse it
+			source = urllib.request.urlopen('https://www.canopygrowth.com/investors/news-releases/').read()
+			soup = bs.BeautifulSoup(source,'lxml')
+			
+			#for each hyperlink we check if its news and if we've seen it before
+			for url in soup.find_all('a'):
+				if is_news(url) and is_recent(url):
+					#here we send an email if its new. For this to work you will need the pw
+					try:
+						msg = "Hey Josh,\n\nA news release from Canopy Growth has been posted:\n\n{}\n\nYour friend,\nCanopy Updates".format(url.get('href'))
+						send_email('canopy.updates@gmail.com','nickandjosh','jlevesque11@gmail.com','Canopy Growth News Release',msg)
+						append_url(url)
+						print("E-mail sent! {}".format(datetime.datetime.now()))
+					except:
+						print("Error sending email! Check e-mail and password and run again! {}\n".format(datetime.datetime.now()))
+		except:
+			print("I had an accident :( {}".format(datetime.datetime.now()))
+		finally:
+			time.sleep(60*5)		
